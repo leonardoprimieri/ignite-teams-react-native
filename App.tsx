e920f5bd2c15f607b7cfb8651aac8da11d8c5da4
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-02-21 16:47:25 -0300

    chore: add expo fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
 import { GroupsScreen } from "@screens/index";
 import theme from "@theme/index";
 import { ThemeProvider } from "styled-components";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { ActivityIndicator } from "react-native";
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <GroupsScreen />
