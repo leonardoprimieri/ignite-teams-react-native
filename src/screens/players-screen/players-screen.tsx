7884d3340211fa4e2498350c84183cbed6a7ca58
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-02-22 10:21:59 -0300

    feat: create form wrapper for input and button

--- src/screens/players-screen/players-screen.tsx
+++ src/screens/players-screen/players-screen.tsx
@@ -1,17 +1,21 @@
-import { ButtonIcon, Header, ScreenTitle } from "@components/index"
-import { Container } from "./players-screen-styles"
+import { ButtonIcon, Header, ScreenTitle, TextInput } from "@components/index"
+import { Container, Form } from "./players-screen-styles"
 
 export const PlayersScreen = () => {
   return (
     <Container>
       <Header shouldShowBackButton />
-
       <ScreenTitle
         title="Nome da turma"
         subtitle="Adicione a galera e separe os times"
       />
-
-      <ButtonIcon icon="home" />
+      <Form>
+        <TextInput
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+        <ButtonIcon icon="add" />
+      </Form>
     </Container>
   )
 }
