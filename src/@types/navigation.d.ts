eb0859ab5593314dfee5fec6310243935d9f5bd3
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-02-22 21:16:43 -0300

    feat: add types on routes

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      'create-group': undefined
+      groups: undefined
+      players: {
+        group: string
+      }
+    }
+  }
+}
--- src/screens/groups-screen/groups-screen.tsx
+++ src/screens/groups-screen/groups-screen.tsx
@@ -1,10 +1,17 @@
 import { Button, EmptyList, GroupCard, Header, ScreenTitle } from "@components/index"
+import { useNavigation } from "@react-navigation/native"
 import { useState } from "react"
 import { FlatList } from "react-native"
 import { Container } from "./groups-screen-styles"
 
 export const GroupsScreen = () => {
   const [groups, setGroups] = useState([])
+
+  const navigation = useNavigation()
+
+  const handleNewGroup = () => {
+    navigation.navigate('create-group')
+  }
 
   return (
     <Container>
@@ -19,7 +26,7 @@ export const GroupsScreen = () => {
         contentContainerStyle={!groups.length && { flex: 1 }}
         ListEmptyComponent={() => <EmptyList message="Que tal cadastrar a primeira turma?" />}
       />
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   )
 }
