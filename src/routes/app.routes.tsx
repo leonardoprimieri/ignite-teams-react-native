527707da05e6f576ed1a295603d16a6af3222e05
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-02-22 21:05:54 -0300

    feat: create AppRoutes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,14 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { GroupsScreen, CreateGroupScreen, PlayersScreen } from "@screens/index";
+
+const { Navigator, Screen } = createNativeStackNavigator()
+
+export const AppRoutes = () => {
+  return (
+    <Navigator>
+      <Screen name="groups" component={GroupsScreen} />
+      <Screen name="create-group" component={CreateGroupScreen} />
+      <Screen name="players" component={PlayersScreen} />
+    </Navigator>
+  )
+}
